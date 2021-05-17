f0baf7cffce2f86d7152e037b0b03f10d2e62218
vinh <dcamvinh96@gmail.com>
2021-05-17 20:45:42 +0700

    Create skeleton for Relationship

---
+++ chat-backend/api/Interfaces/IRelationshipRepository.cs
@@ -0,0 +1,19 @@
+﻿using api.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    interface IRelationshipRepository
+    {
+        void sendRequest(int senderId, int receiverId, string status);
+
+        void rejectRequest(int id);
+        void acceptRequest(int id);
+        Task<RelationshipDto> getSingleRequest(int receiverId);
+
+        Task<IEnumerable<RelationshipDto>> getAllRequest(int receiverId);
+    }
+}
